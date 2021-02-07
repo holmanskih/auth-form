fe7dcaff68e271632f4034d879093a105a51af8a
evgeniy.holmanskih <evg.holmanskih@gmail.com>
2021-02-07 11:34:55 +0200

    add validation

--- form.js
+++ form.js
@@ -1,4 +1,35 @@
-const logIn = () => {
-    console.log('on press');
+const login = () => {
+    let userNameInput = document.getElementById('auth_user_name').value;
+    let passwordInput = document.getElementById('auth_password').value;
+
+    let isAuthCheckboxChecked = document.getElementById('auth_checkbox').checked;
+
+    // call validation function to get error string
+    let validationError = validate(userNameInput, passwordInput, isAuthCheckboxChecked);
+    if (validationError == undefined) {
+        // login process
+
+    } else {
+        // display error to the user
+
+    }
 }
 
+const validate = (userName, password, isPolicyChecked) => {
+    if(userName != '' && password != '' && isPolicyChecked) {
+        // validate user name on regex 
+        let regex = new RegExp('[a-zA-Z]');
+        let isUserName = regex.test(userName);
+        if (isUserName == false) {
+            return 'Please enter user name by only characters.';
+        }
+
+        // validate password on length 
+        let isPassword = (password.length > 5) ? true : false;
+        if (isPassword == false) {
+            return 'Please enter password with length greater then 5.';
+        }
+    } else {
+        return 'Please enter and check all fields!';
+    }
+}
